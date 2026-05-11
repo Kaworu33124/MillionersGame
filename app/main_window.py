6e4f71a478026ce007c5c73e6bcc8a4df184e840
Kaworu33124 <dima.burkov.08@mail.ru>
2026-05-11 15:26:11 +0300

    add bank

--- app/main_window.py
+++ app/main_window.py
@@ -6,6 +6,6 @@ class MainWindow:
     def __init__(self):
         self.window = Tk()
         self.window.title("Кто хочет стать миллионером")
-        self.window.geometry("1280x720")
+        self.window.geometry("1350x720")
         Graphics.setup(self.window)
         self.window.mainloop()
--- app/widgets/buttons.py
+++ app/widgets/buttons.py
@@ -4,6 +4,7 @@ class button_setup:
     def __init__(self, window):
         self.window = window
         self.buttons()
+        self.bank_label()
         self.question_label()
 
     def buttons(self):
@@ -22,3 +23,16 @@ class button_setup:
     def question_label(self):
         question = tk.Label(self.window, text='', bg='#5B6E7A',fg='white', width=65, height=2, font=("Arial", 20))
         question.place(x=125, y=350)
+
+    def bank_label(self):
+        bank = [
+            '1.000.000', '500.000', '250.000', "125.000",
+            "64.000", "32.000", "16.000", "8.000",
+            "4.000", "2.000", "1.000", "500",
+            "300", "200", "100"
+        ]
+        location_y = 10
+        for i in bank:
+            i = tk.Label(self.window, text=i, bg='#10069F',fg='white', width=8, height=1, font=("Arial", 20))
+            i.place(x=1200, y=location_y)
+            location_y +=30
