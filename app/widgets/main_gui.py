df59fb6f6e8a4f8cefb3d83355378e11608d8886
Kaworu33124 <dima.burkov.08@mail.ru>
2026-05-11 16:58:21 +0300

    visualization of the correct answer

--- app/widgets/main_gui.py
+++ app/widgets/main_gui.py
@@ -7,6 +7,10 @@ class button_setup:
     def __init__(self, window):
         self.window = window
         self.data = parse_json_basic('C:/Users/Admin/Desktop/pyton/millioner/app/questions.json')
+        self.btn1 = None  
+        self.btn2 = None
+        self.btn3 = None
+        self.btn4 = None
         self.question()
         self.buttons()
         self.bank_label()
@@ -14,16 +18,16 @@ class button_setup:
 
 
     def buttons(self):
-        btn1 = tk.Button(self.window, text=self.opt1, bg='#5B6E7A', width=40, height=3, bd=2, relief=tk.SOLID, font=("Arial", 10))
+        btn1 = tk.Button(self.window, text=self.opt1, bg='#5B6E7A', width=40, height=3, bd=2, relief=tk.SOLID, font=("Arial", 10), command=lambda: self.check_answer(btn1))
         btn1.place(x=220, y=580)
 
-        btn2 = tk.Button(self.window, text=self.opt2, bg='#5B6E7A', width=40, height=3, bd=2, relief=tk.SOLID, font=("Arial", 10))
+        btn2 = tk.Button(self.window, text=self.opt2, bg='#5B6E7A', width=40, height=3, bd=2, relief=tk.SOLID, font=("Arial", 10), command=lambda: self.check_answer(btn2))
         btn2.place(x=220, y=480)
 
-        btn3 = tk.Button(self.window, text=self.opt3, bg='#5B6E7A', width=40, height=3, bd=2, relief=tk.SOLID, font=("Arial", 10))
+        btn3 = tk.Button(self.window, text=self.opt3, bg='#5B6E7A', width=40, height=3, bd=2, relief=tk.SOLID, font=("Arial", 10), command=lambda:self.check_answer(btn3))
         btn3.place(x=720, y=580)
 
-        btn4 = tk.Button(self.window, text=self.opt4, bg='#5B6E7A', width=40, height=3, bd=2, relief=tk.SOLID, font=("Arial", 10))
+        btn4 = tk.Button(self.window, text=self.opt4, bg='#5B6E7A', width=40, height=3, bd=2, relief=tk.SOLID, font=("Arial", 10), command=lambda: self.check_answer(btn4))
         btn4.place(x=720, y=480)
 
     def question_label(self):
@@ -47,4 +51,9 @@ class button_setup:
         self.x = random.randint(1, 10)
         que_list = self.data['questions'][self.x]['options']
         self.que = self.data['questions'][self.x]['text']
+        self.anw = self.data['questions'][self.x]['answer']
         self.opt1, self.opt2, self.opt3, self.opt4  = que_list
+
+    def check_answer(self, name_button):
+        if name_button.cget('text') == self.anw:
+            name_button.config(bg='green')
