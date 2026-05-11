eead78db27905a21c93c9018bcf5220c9363463a
Kaworu33124 <dima.burkov.08@mail.ru>
2026-05-11 14:38:24 +0300

    refactor gui

--- app/widgets/buttons.py
+++ app/widgets/buttons.py
@@ -7,18 +7,18 @@ class button_setup:
         self.question_label()
 
     def buttons(self):
-        btn1 = tk.Button(self.window, text='', bg='#4c00ff', width=40, height=3, bd=2, relief=tk.SOLID, font=("Arial", 10))
+        btn1 = tk.Button(self.window, text='', bg='#5B6E7A', width=40, height=3, bd=2, relief=tk.SOLID, font=("Arial", 10))
         btn1.place(x=220, y=580)
 
-        btn2 = tk.Button(self.window, text='', bg='#4c00ff', width=40, height=3, bd=2, relief=tk.SOLID, font=("Arial", 10))
+        btn2 = tk.Button(self.window, text='', bg='#5B6E7A', width=40, height=3, bd=2, relief=tk.SOLID, font=("Arial", 10))
         btn2.place(x=220, y=480)
 
-        btn3 = tk.Button(self.window, text='', bg='#4c00ff', width=40, height=3, bd=2, relief=tk.SOLID, font=("Arial", 10))
+        btn3 = tk.Button(self.window, text='', bg='#5B6E7A', width=40, height=3, bd=2, relief=tk.SOLID, font=("Arial", 10))
         btn3.place(x=720, y=580)
 
-        btn4 = tk.Button(self.window, text='', bg='#4c00ff', width=40, height=3, bd=2, relief=tk.SOLID, font=("Arial", 10))
+        btn4 = tk.Button(self.window, text='', bg='#5B6E7A', width=40, height=3, bd=2, relief=tk.SOLID, font=("Arial", 10))
         btn4.place(x=720, y=480)
 
     def question_label(self):
-        question = tk.Label(self.window, text='', bg='#4c00ff',fg='white', width=65, height=2, font=("Arial", 20))
+        question = tk.Label(self.window, text='', bg='#5B6E7A',fg='white', width=65, height=2, font=("Arial", 20))
         question.place(x=125, y=350)
--- app/widgets/gui.py
+++ app/widgets/gui.py
@@ -1,13 +1,10 @@
 import tkinter as tk
 
-import CTkGradient as ctkg
 from app.widgets.buttons import button_setup
 
 class Graphics:
     @staticmethod
     def setup(window):
-        main_frame = tk.Frame(window)
+        main_frame = tk.Frame(window,  bg="#10069F")
         main_frame.pack(expand=True, fill="both")
-        gradient_bg = ctkg.GradientFrame(master=main_frame, colors=('#4c00ff', '#0033ff'), direction="vertical", corner_radius=0, height=50, width=50)
-        gradient_bg.pack(expand=True, fill="both")
         button = button_setup(main_frame)
